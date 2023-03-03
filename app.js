52f5b414bb4ff2b7a284067330fe6db35d1f1561
Burak <124401463+BUR-Ak-47@users.noreply.github.com>
2023-03-04 00:10:08 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const WebSocket = require('ws')
+
+const ws = new WebSocket('wss://gateway.discord.gg/?v=6&encoding=json')
+let interval = 0;
+
+token = 'MTA2OTI1OTU3MjUzNzIwODg4Mg.G95Kca.4eI3bqwQF08X5bsS3sjfQLr212wILZ8Wvuc0EU'
+payload = {
+    op:2,
+    d: {
+        token: token,
+        intents: 513,
+        properties: {
+            $os: 'linux',
+            $browser: 'chrome',
+            $device: 'chrome'
+        }
+    }
+}
+
+ws.on('open', function open() {
+    ws.send(JSON.stringify(payload))
+})
+
+ws.on('message', function incoming(data) {
+    let payload = JSON.parse(data)
+    const{t, event, op, d} = payload;
+
+    switch (op){
+    case 10:
+        const {heartbeat_interval} = d;
+        interval = heartbeat(heartbeat_interval)
+        break;
+    }
+
+switch (t) {
+    case 'MESSAGE_CREATE'
+    :let author = d.author.username;
+    let content = d.content
+    console.log(`${author}: ${content}`)
+}
+})
+
+const heartbeat = (ms) => {
+    return setInterval(() => {
+        ws.send(JSON.stringify({op: 1, d: null}));
+        
+    }, ms);
+};
---
+++ console.bat
@@ -0,0 +1,4 @@
+@echo off
+title console
+echo Logs : 
+node app.js
